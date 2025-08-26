b4c4fb2d6f9e8994f66d9ac181123968f7523104
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2025-08-26 13:53:26 +0800

    Initialize db models repository

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="zhai_db_models",
+    version="0.1.0",
+    description="Database models for ZeroHungerAI",
+    author="R G B",
+    author_email="gabrielbongocan@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "geoalchemy2>=0.18.0",
+        "sqlalchemy>=2.0.43",
+    ],
+    python_requires=">=3.11",
+    include_package_data=True,
+)
