a3f0bef702e02aa5d45b5096a16d0e025e3211f7
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2025-08-27 19:52:56 +0800

    Create article download and concept uri models

---
+++ migrations/versions/9c3dbacf4eff_create_article_download_and_concept_uri_.py
@@ -0,0 +1,86 @@
+"""Create article download and concept uri models
+
+Revision ID: 9c3dbacf4eff
+Revises: 077a21dbd5de
+Create Date: 2025-08-27 19:48:14.989909
+
+"""
+
+from typing import Sequence, Union
+
+import geoalchemy2
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "9c3dbacf4eff"
+down_revision: Union[str, Sequence[str], None] = "077a21dbd5de"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "concept_uris",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("concept_uri", sa.String(), nullable=False),
+        sa.Column("geo_names_id", sa.Integer(), nullable=True),
+        sa.Column(
+            "geom",
+            geoalchemy2.types.Geometry(
+                geometry_type="POINT",
+                srid=4326,
+                dimension=2,
+                from_text="ST_GeomFromEWKT",
+                name="geometry",
+            ),
+            nullable=True,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("concept_uri"),
+    )
+    op.create_index(
+        "idx_concept_uris_latlon",
+        "concept_uris",
+        ["geom"],
+        unique=False,
+        postgresql_using="gist",
+    )
+    op.create_table(
+        "article_downloads",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("uri", sa.String(), nullable=False),
+        sa.Column("cloud_uri", sa.String(), nullable=False),
+        sa.Column("published_at", sa.DateTime(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["uri"],
+            ["article_uri_results.uri"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("cloud_uri"),
+        sa.UniqueConstraint("uri"),
+    )
+    op.create_table(
+        "article_concept_association",
+        sa.Column("article_uri", sa.String(), nullable=True),
+        sa.Column("concept_uri", sa.String(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ["article_uri"],
+            ["article_downloads.uri"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["concept_uri"],
+            ["concept_uris.concept_uri"],
+        ),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("article_concept_association")
+    op.drop_table("article_downloads")
+    op.drop_index(
+        "idx_concept_uris_latlon", table_name="concept_uris", postgresql_using="gist"
+    )
+    op.drop_table("concept_uris")
--- zhai_db_models/articles.py
+++ zhai_db_models/articles.py
@@ -1,3 +1,4 @@
+from geoalchemy2 import Geometry
 from sqlalchemy import (
     Column,
     DateTime,
@@ -5,10 +6,12 @@ from sqlalchemy import (
     ForeignKey,
     Integer,
     String,
+    Table,
     UniqueConstraint,
     func,
 )
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import relationship
 
 from .base import Base
 
@@ -30,9 +33,42 @@ class ArticleUriResult(Base):
     weight = Column(Float, nullable=False)
     page_id = Column(Integer, nullable=False)
     queried_at = Column(DateTime, nullable=False)
-    queried_at = Column(DateTime, nullable=False)
 
 
+article_concept_association = Table('article_concept_association', Base.metadata,
+    Column('article_uri', String, ForeignKey('article_downloads.uri')),
+    Column('concept_uri', String, ForeignKey('concept_uris.concept_uri'))
+)
+
+class ArticleDownload(Base):
+    __tablename__ = "article_downloads"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    uri = Column(String, ForeignKey("article_uri_results.uri"), nullable=False, unique=True)
+    cloud_uri = Column(String, unique=True, nullable=False)
+    published_at = Column(DateTime, nullable=False)
+
+    concepts = relationship('ConceptUri', secondary=article_concept_association, back_populates='article_downloads')
+
+    def __repr__(self):
+        return f"<ArticleDownload(uri='{self.uri}')>"
+
+
+class ConceptUri(Base):
+    __tablename__ = "concept_uris"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    concept_uri = Column(String, unique=True, nullable=False)
+    geo_names_id = Column(Integer, nullable=True)
+    geom = Column(Geometry('POINT', srid=4326), nullable=True)
+
+    articles = relationship('ArticleDownload',  secondary=article_concept_association, back_populates='concept_uris')
+
+    def __repr__(self):
+        return f"<ConceptUri(concept_uri='{self.concept_uri}')>"
+
+
+# todo: decommission and reclaim table name for ArticleUriResult
 class ArticleUri(Base):
     __tablename__ = "article_uris"
 
