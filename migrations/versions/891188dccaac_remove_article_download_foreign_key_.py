a413b268692407f0d049db2c9d681d7dcc6e2780
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2025-09-09 23:39:17 +0800

    Revise drop constraint to cascade

--- migrations/versions/891188dccaac_remove_article_download_foreign_key_.py
+++ migrations/versions/891188dccaac_remove_article_download_foreign_key_.py
@@ -7,9 +7,8 @@ Create Date: 2025-09-09 11:13:34.019188
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '891188dccaac'
@@ -20,17 +19,15 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.drop_constraint(op.f('article_downloads_uri_key'), 'article_downloads', type_='unique')
+    op.execute("ALTER TABLE article_downloads DROP CONSTRAINT article_downloads_uri_key CASCADE")
     op.create_index(op.f('ix_article_downloads_uri'), 'article_downloads', ['uri'], unique=True)
     op.drop_constraint(op.f('article_downloads_uri_fkey'), 'article_downloads', type_='foreignkey')
-    op.drop_constraint(op.f('article_uris_uri_key'), 'article_uris', type_='unique')
     op.create_index(op.f('ix_article_uris_uri'), 'article_uris', ['uri'], unique=False)
 
 
 def downgrade() -> None:
     """Downgrade schema."""
     op.drop_index(op.f('ix_article_uris_uri'), table_name='article_uris')
-    op.create_unique_constraint(op.f('article_uris_uri_key'), 'article_uris', ['uri'], postgresql_nulls_not_distinct=False)
     op.create_foreign_key(op.f('article_downloads_uri_fkey'), 'article_downloads', 'article_uris', ['uri'], ['uri'])
     op.drop_index(op.f('ix_article_downloads_uri'), table_name='article_downloads')
     op.create_unique_constraint(op.f('article_downloads_uri_key'), 'article_downloads', ['uri'], postgresql_nulls_not_distinct=False)
