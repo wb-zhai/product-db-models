22f03b2b3a1136cb2ad3b0a949477b19d1d45b60
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2025-09-09 16:55:59 +0800

    Rename var to concept_enum

--- migrations/versions/00b44e406324_add_concept_type_to_concepturi.py
+++ migrations/versions/00b44e406324_add_concept_type_to_concepturi.py
@@ -17,15 +17,15 @@ down_revision: Union[str, Sequence[str], None] = '4dd6973884db'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-user_status_enum = postgresql.ENUM('wiki', 'person', 'loc', 'org', name='concept_enum')
+concept_enum = postgresql.ENUM('wiki', 'person', 'loc', 'org', name='concept_enum')
 
 def upgrade() -> None:
     """Upgrade schema."""
-    user_status_enum.create(op.get_bind())
+    concept_enum.create(op.get_bind())
     op.add_column('concept_uris', sa.Column('concept_type', postgresql.ENUM('WIKI', 'PERSON', 'LOCATION', 'ORGANIZATION', name='concept_enum'), nullable=False))
 
 
 def downgrade() -> None:
     """Downgrade schema."""
     op.drop_column('concept_uris', 'concept_type')
-    user_status_enum.drop(op.get_bind())
+    concept_enum.drop(op.get_bind())
