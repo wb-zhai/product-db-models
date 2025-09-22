554e5c3f551f242639d1d47dfe0432639ae9c761
Jerome <jerome-white@users.noreply.github.com>
2025-09-22 09:29:09 +0530

    Enforce uniqueness of article-concept pairs (#17)
    * Add uniqueness constraint to article-concept association table
    
    * Alembic revision file
    
    * Remove Alembic comments
    
    * Remove migrations not relate to article-concept association
    
    * Add naming for foreign key constraint

---
+++ migrations/versions/3734a324ea3b_enforce_unique_article_concept_.py
@@ -0,0 +1,40 @@
+"""enforce unique article-concept association
+
+Revision ID: 3734a324ea3b
+Revises: 3a640e38e6f4
+Create Date: 2025-09-19 14:55:54.624852
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '3734a324ea3b'
+down_revision: Union[str, Sequence[str], None] = '3a640e38e6f4'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.alter_column('article_concept_association', 'article_uri',
+               existing_type=sa.VARCHAR(),
+               nullable=False)
+    op.alter_column('article_concept_association', 'concept_uri',
+               existing_type=sa.VARCHAR(),
+               nullable=False)
+    op.create_foreign_key('fk_article_concept_association_uris', 'article_concept_association', 'article_downloads', ['article_uri'], ['uri'])
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint('fk_article_concept_association_uris', 'article_concept_association', type_='foreignkey')
+    op.alter_column('article_concept_association', 'concept_uri',
+               existing_type=sa.VARCHAR(),
+               nullable=True)
+    op.alter_column('article_concept_association', 'article_uri',
+               existing_type=sa.VARCHAR(),
+               nullable=True)
