d8cdd0f8455b5cddb503107f40c76b594fc5ade9
Jerome White <jerome@thewex.net>
2025-10-16 18:28:35 +0530

    Ensure geotaxonomy's admin code is unique

--- zhai_db_models/geotaxonomy.py
+++ zhai_db_models/geotaxonomy.py
@@ -25,8 +25,9 @@ class Geotaxonomy(Base):
             postgresql_using="gin",
             postgresql_ops={"adm_name": "gin_trgm_ops"},
         ),
+        UniqueConstraint("adm_code", name="unique_adm_code"),
         # comment the line below to generate migrations for this table
-        {"info": {"skip_autogenerate": True}},
+        # {"info": {"skip_autogenerate": True}},
     )
 
     id = Column(Integer, primary_key=True)
