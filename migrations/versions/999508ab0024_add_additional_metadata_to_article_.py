7b41385c9e2d1ca6ac7acb7bccbb47e370a4e9a8
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2025-10-27 21:06:23 +0800

    Drop enum type on downgrade

--- migrations/versions/999508ab0024_add_additional_metadata_to_article_.py
+++ migrations/versions/999508ab0024_add_additional_metadata_to_article_.py
@@ -7,8 +7,8 @@ Create Date: 2025-10-27 09:45:40.984321
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
@@ -21,9 +21,8 @@ article_enum = postgresql.ENUM('pr', 'blog', 'news', name='article_enum')
 
 def upgrade() -> None:
     """Upgrade schema."""
-    article_enum.create(op.get_bind())
-
     op.create_foreign_key('fk_article_concept_association_concept_uri', 'article_concept_association', 'concept_uris', ['concept_uri'], ['concept_uri'])
+    article_enum.create(op.get_bind())
     op.add_column('article_downloads', sa.Column('source_uri', sa.String(), nullable=True))
     op.add_column('article_downloads', sa.Column('is_duplicate', sa.Boolean(), nullable=True))
     op.add_column('article_downloads', sa.Column('article_type', article_enum, nullable=True))
@@ -38,6 +37,5 @@ def downgrade() -> None:
     op.drop_column('article_downloads', 'article_type')
     op.drop_column('article_downloads', 'is_duplicate')
     op.drop_column('article_downloads', 'source_uri')
+    article_enum.drop(op.get_bind())
     op.drop_constraint('fk_article_concept_association_concept_uri', 'article_concept_association', type_='foreignkey')
-
-    article_enum.create(op.get_bind())
