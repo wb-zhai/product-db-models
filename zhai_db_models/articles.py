769e2c5302cee699901778bc0236c5112a74d40f
Jerome White <jerome@thewex.net>
2025-11-03 11:36:32 +0530

    Better tag tracker table naming

--- zhai_db_models/articles.py
+++ zhai_db_models/articles.py
@@ -184,13 +184,13 @@ class ArticleLocationTags(Base):
     strength = Column(Float)
     tag_method_url = Column(String, nullable=False, primary_key=True)
 
-class ArticleTagsTracker(Base):
-    __tablename__ = "article_tags_tracker"
+class TaggedArticle(Base):
+    __tablename__ = "tagged_article"
     __table_args__ = (
         PrimaryKeyConstraint(
             "article_uri",
             "tag_method_url",
-            name="pk_article_tags_tracker",
+            name="pk_tagged_article",
         ),
     )
 
@@ -198,7 +198,7 @@ class ArticleTagsTracker(Base):
         String,
         ForeignKey(
             "article_downloads.uri",
-            name="fk_article_tags_tracker_article_uri",
+            name="fk_tagged_article_article_uri",
         ),
         nullable=False,
         primary_key=True,
