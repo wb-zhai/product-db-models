1fd0e43b25591039c43f5c1f6119d848efce29f2
Renzo Gabriel Bongocan <gabrielbongocan@gmail.com>
2026-02-23 14:34:41 +0800

    Rename module into food_insecurity; alembic-ignore FoodInsecurityScore

--- zhai_db_models/food_insecurity.py
+++ zhai_db_models/food_insecurity.py
@@ -17,26 +17,10 @@ from sqlalchemy.orm import relationship
 from .base import Base
 
 
-# TODO: delete this table once endpoints are refactored to use FoodInsecurityScore
-class FoodSecurityDummy(Base):
-    __tablename__ = "food_security_dummy"
-    __table_args__ = ({"info": {"skip_autogenerate": True}},)
-
-    id = Column(Integer, primary_key=True)
-    country = Column(String, nullable=False)
-    adm_level = Column(Integer, nullable=False)
-    adm0_code = Column(String, nullable=False)
-    adm1_code = Column(String, nullable=True)
-    adm2_code = Column(String, nullable=True)
-    adm_code = Column(String, nullable=False)
-    year = Column(Integer, nullable=False)
-    month = Column(Integer, nullable=False)
-    ipc_score = Column(REAL, nullable=False)
-
-
 class FoodInsecurityScore(Base):
     __tablename__ = "food_insecurity_scores"
     __table_args__ = (
+        {"info": {"skip_autogenerate": True}},
         PrimaryKeyConstraint(
             "adm_code", "year_month", name="pk_food_insecurity_scores"
         ),
