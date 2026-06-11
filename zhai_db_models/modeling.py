69e62f5db194c1d1440e764a2067b92be0879867
Jerome White <jerome@thewex.net>
2026-06-11 19:14:46 +0530

    Add version and data source

--- zhai_db_models/modeling.py
+++ zhai_db_models/modeling.py
@@ -1,6 +1,7 @@
 import enum
 
 from sqlalchemy import (
+    Boolean,
     Column,
     DateTime,
     Float,
@@ -88,10 +89,18 @@ class ModelingRegressionEvaluation(Base):
     )
     score = Column(Float, nullable=False)
 
+class DataSourceType(enum.Enum):
+    dataset = "dataset"
+    modeling = "modeling"
+    inference = "inference"
+
 class ModelingFrontendResults(Base):
     __tablename__ = "modeling_regression_evaluation"
 
-    date = Column(DateTime(timezone=True), nullable=False)
+    date = Column(
+        DateTime(timezone=True),
+        nullable=False,
+    )
     adm_code = Column(
         String,
         ForeignKey(
@@ -101,6 +110,26 @@ class ModelingFrontendResults(Base):
         ),
         nullable=False,
     )
-    feature_type = Column(String)
+
+    version = Column(
+        UUID(as_uuid=True),
+        nullable=False,
+        index=True,
+    )
+    data_source = Column(
+        ENUM(
+            DataSourceType,
+            name="data_source_enum",
+            create_type=True,
+        ),
+        nullable=False,
+        index=True,
+    )
+
+    feature_type = Column(
+        String,
+        nullable=False,
+        index=True,
+    )
     feature_name = Column(String)
     feature_value = Column(Float)
